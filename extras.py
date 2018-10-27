290acd0a052a440bbeb091ec839eaef0e80a23f7
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2018-10-27 13:54:47 +0530

    Update extras.py

--- extras.py
+++ extras.py
@@ -4,33 +4,6 @@ from math import sin, cos, sqrt, atan2, radians
 from threading import Timer
 import time
 
-
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-
 # approximate radius of earth in km
 R = 6373.0
 
